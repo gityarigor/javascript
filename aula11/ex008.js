63f78d2da0db4a2436cf3d984520965845bf1b88
Igor Ramos <88554398+gityarigor@users.noreply.github.com>
2023-02-15 12:46:38 -0300

    exercicios com condições pt1

---
+++ aula11/ex008.js
@@ -0,0 +1,14 @@
+var vel = 59
+console.log(`A velocidade do seu carro é ${vel}`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
+
+/* Condição simples
+
+if (condição) {
+
+}
+
+*/
