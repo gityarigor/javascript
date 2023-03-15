96cdc2a4a6e272eee4465caed5f4f593ab202c0b
Igor Ramos <88554398+gityarigor@users.noreply.github.com>
2023-03-15 04:27:51 -0300

    repetição for

---
+++ aula14/ambiente2.js
@@ -0,0 +1,15 @@
+/*
+var c = 1
+console.log(`Contando`)
+while (c <= 10 ) {
+    console.log(`${c}`)
+    c++
+}
+*/
+//-------------------------------
+
+console.log(`Contando`)
+for (var c = 1; c <= 10; c++) {
+    console.log(`${c}`)
+}
+console.log(`FIM`)
